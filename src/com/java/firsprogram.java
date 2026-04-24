b69c9ae38e0451215e20c3a7426fccab0037d2a0
sunilmonster450-a11y <sunilmonster450@gmail.com>
2026-04-24 12:24:40 +0530

    "Picchi pooku"

--- src/com/java/firsprogram.java
+++ src/com/java/firsprogram.java
@@ -4,7 +4,8 @@ public class firsprogram
 {
 	public static void main(String [] args)
 	{
-		System.out.println("hi maaster sunil kumar");
+		System.out.println("hi maaster sunil kumar ");
+		System.out.println("hi maaster sunil kumar gaaru ");
 
 }
 }
