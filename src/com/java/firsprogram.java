572b02da431e1a6bcdf19d08c56308421a77bcf5
sunilmonster450-a11y <sunilmonster450@gmail.com>
2026-04-24 11:44:39 +0530

    "iam commit"

--- src/com/java/firsprogram.java
+++ src/com/java/firsprogram.java
@@ -1,5 +1,11 @@
 package com.java;
 
-public class firsprogram {
+public class firsprogram 
+{
+	public static void main(String [] args)
+	{
+		System.out.println("hi mister sunil kumar");
 
 }
+}
+
